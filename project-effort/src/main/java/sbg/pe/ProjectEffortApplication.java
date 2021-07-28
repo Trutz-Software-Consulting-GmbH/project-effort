483e9c2b7ea129f2440ffc9c25221d67c06c6669
Christian Trutz <info@trutz-software-consulting.de>
2021-07-28 14:28:15 +0200

    ApplicationAvailability wird direkt ausgeben

--- project-effort/src/main/java/sbg/pe/ProjectEffortApplication.java
+++ project-effort/src/main/java/sbg/pe/ProjectEffortApplication.java
@@ -8,27 +8,25 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.availability.ApplicationAvailability;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ProjectEffortApplication implements ApplicationRunner {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ProjectEffortApplication.class);
 
-	private final ApplicationContext applicationContext;
+	private final ApplicationAvailability availability;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectEffortApplication.class, args);
 	}
 
 	@Autowired
-	public ProjectEffortApplication(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
+	public ProjectEffortApplication(ApplicationAvailability availability) {
+		this.availability = availability;
 	}
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		ApplicationAvailability availability = applicationContext.getBean(ApplicationAvailability.class);
 		LOG.info("Ready: {}, Live: {}", availability.getReadinessState(), availability.getLivenessState());
 	}
 
