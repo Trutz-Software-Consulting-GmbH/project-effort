c7a3cd784a5a03c64d3b8042ceb3091f668bf486
Christian Trutz <info@trutz-software-consulting.de>
2021-07-28 14:49:42 +0200

    Verwendung von Spring Boot Tests.

--- project-effort/src/test/java/sbg/pe/ProjectEffortApplicationTests.java
+++ project-effort/src/test/java/sbg/pe/ProjectEffortApplicationTests.java
@@ -1,13 +1,19 @@
 package sbg.pe;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjectEffortApplicationTests {
 
+	@Autowired
+	private ProjectEffortApplication app;
+
 	@Test
-	void contextLoads() {
+	void shouldBeNotNull() {
+		Assertions.assertNotNull(app);
 	}
 
 }
